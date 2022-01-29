4c861b780bf2f72db38aa478006ad60b32d7aecd
waldorfxx <w.waldorfxx@gmail.com>
2022-01-29 15:31:39 +0900

    #1 Update Gallery

--- vue/src/components/Gallery.vue
+++ vue/src/components/Gallery.vue
@@ -5,44 +5,44 @@
     <div class="row">
         <div class="column">
             
-          <a data-fancybox="gallery" href="../assets/img/img-01.jpg">
+          <a data-fancybox="gallery" href="https://bit.ly/35AqhRb">
             <img src="../assets/thumb/thumbnail-01.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" href="https://bit.ly/32EcwQd">
             <img src="../assets/thumb/thumbnail-02.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3KTaGfF">
             <img src="../assets/thumb/thumbnail-03.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3rgSxkd">
             <img src="../assets/thumb/thumbnail-04.jpg"></a>
         </div>
 
         <div class="column">
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3r8bYeU">
             <img src="../assets/thumb/thumbnail-05.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/34eH9MC">
             <img src="../assets/thumb/thumbnail-06.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/32DctnC">
             <img src="../assets/thumb/thumbnail-07.jpg"></a>
         </div>
 
         <div class="column">
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3IIZPTt">
             <img src="../assets/thumb/thumbnail-08.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3Hbdliu">
             <img src="../assets/thumb/thumbnail-09.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/32C8986">
             <img src="../assets/thumb/thumbnail-10.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3u8cRWK">
             <img src="../assets/thumb/thumbnail-11.jpg"></a>
         </div>
 
         <div class="column">
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3KMZLUC">
             <img src="../assets/thumb/thumbnail-12.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3r9VK50">
             <img src="../assets/thumb/thumbnail-13.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3g24d3R">
             <img src="../assets/thumb/thumbnail-14.jpg"></a>
-          <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1024x768">
+          <a data-fancybox="gallery" data-src="https://bit.ly/3u7Cxmn">
             <img src="../assets/thumb/thumbnail-15.jpg"></a> 
         </div>
     
@@ -54,6 +54,7 @@
 export default {
     name: 'Gallery'
 }
+
 </script>
 
 <style>
