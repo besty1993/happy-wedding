bb82b6bda8859fa2dcd0734329a6880f68751746
besty1993 <musso114@naver.com>
2022-01-29 17:05:02 +0900

    #1 add vuesax

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax)
+
 createApp(App).mount('#app')
