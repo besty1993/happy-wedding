a9491b643ed08e3ddb73173f08e8d57e42d05b71
besty1993 <musso114@naver.com>
2022-02-02 11:59:14 +0900

    Donation to Account

---
+++ vue/src/components/Account.vue
@@ -0,0 +1,41 @@
+<template>
+    <h2>마음 보내실 곳</h2>
+    <AccountItem />
+</template>
+
+<script>
+import AccountItem from './AccountItem'
+
+export default {
+    name: 'Account',
+    components: {
+        AccountItem,
+    },
+    data() {
+        return {
+            groom: [
+                {
+                    name: '양창은',
+                    bank: '우리',
+                    account: '1002745899033',
+                },
+                {
+                    name: '양선직',
+                    bank: '',
+                    account: '',
+                }
+            ],
+            bride: [
+                {
+                    name: '세라오스니'
+                    bank: '우리',
+                    account: '',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ vue/src/components/AccountItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AccountItem',
+    props: {
+        name: String,
+        bank: String,
+        account: String,
+    }
+}
+</script>
+
+<style>
+</style>
