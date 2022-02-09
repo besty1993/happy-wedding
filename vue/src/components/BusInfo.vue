ed36be7e5539bb40f5a490e51ccf594e3bf78b96
besty1993 <musso114@naver.com>
2022-02-09 15:00:47 +0900

    Add bus info

--- vue/src/components/BusInfo.vue
+++ vue/src/components/BusInfo.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>Temp Bus Info</div>
+  <vs-row
+    vs-algin="center"
+    vs-justify="center"
+  >
+    <vs-col
+      vs-w="5"
+      vs-align="center"
+      vs-justify="center"
+    >
+      <h3>서울 -> 예식장</h3>
+      <p>장소: 잠실역 4번 출구 방면 롯데월드 제타플렉스점 앞</p>
+      <p>시간: 4월 10일 9:00 AM</p>
+    </vs-col>
+
+    <vs-col
+      vs-w="5"
+      vs-align="center"
+      vs-justify="center"
+    >
+      <h3>예식장 -> 서울</h3>
+      <p>장소: 숲속웨딩공원 주차장</p>
+      <p>시간: 4월 10일 14:00 PM</p>
+    </vs-col>
+  </vs-row>
 </template>
 
 <script>
---
+++ vue/src/components/BusInfoItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name:"BusInfoItem",
+}
+</script>
+
+<style>
+
+</style>
